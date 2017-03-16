ea1a7391b6aee3c829c955be9fb5d49e71e3bd12
Thiago Durante Pires <thdurante@gmail.com>
2017-03-16 18:37:53 -0300

    React Events. goToStore method and binding this. Using ref to fetch input value Video #11

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,11 +2,22 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+    constructor() {
+        super();
+        this.goToStore = this.goToStore.bind(this);
+    }
+
+    goToStore(event) {
+        event.preventDefault();
+        console.log('You changed the url.');
+        console.log(this.storeInput.value);
+    }
+
     render() {
         return (
-            <form className="store-selector">
+            <form className="store-selector" onSubmit={this.goToStore}>
                 <h2>Please enter a store</h2>
-                <input type="text" required placeholder="Store name" defaultValue={getFunName()} />
+                <input type="text" required placeholder="Store name" defaultValue={getFunName()} ref={(input) => { this.storeInput = input }} />
                 <button type="submit">Visit Store</button>
             </form>
         );
