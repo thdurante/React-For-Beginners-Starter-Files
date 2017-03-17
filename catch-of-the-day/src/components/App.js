8a25acf96f3d816b33fed5a506123d2f77c7306b
Thiago Durante Pires <thdurante@gmail.com>
2017-03-17 10:45:11 -0300

    Loading sample fishes Video #14

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component {
 
     constructor() {
         super();
         this.addFish = this.addFish.bind(this);
+        this.loadSamples = this.loadSamples.bind(this);
 
         // Initial State
         this.state = {
@@ -28,6 +30,12 @@ class App extends React.Component {
         this.setState({ fishes }); // Same as this.setState({ fishes: fishes });
     }
 
+    loadSamples() {
+        this.setState({
+            fishes: sampleFishes
+        });
+    }
+
     render() {
         return (
             <div className="catch-of-the-day">
@@ -35,7 +43,7 @@ class App extends React.Component {
                     <Header tagline="Fresh Seafood Market" />
                 </div>
                 <Order />
-                <Inventory addFish={this.addFish} />
+                <Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
             </div>
         );
     }
