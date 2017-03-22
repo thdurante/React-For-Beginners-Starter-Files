03faa089d292ac1848aa9e844a9ba7959671d01f
Thiago Durante Pires <thdurante@gmail.com>
2017-03-22 15:11:24 -0300

    Persisting the state with Firebase Video #18

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+    apiKey: "AIzaSyCHXjjG9C0WXzpm6Lg6i-oRLH1g4jyLQ3A",
+    authDomain: "catch-of-the-day-thiago.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-thiago.firebaseio.com",
+});
+
+export default base;
